d1f65cf7b00878e4fbbb922f7e381989b9722ddc
VycMajoris <ok1907@gmail.com>
2023-01-19 06:44:07 +0300

    Add header and main sections.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-alert */
+const myLibrary = [];
+
 // Constructor
 function Book(title, author, pages, read) {
   this.title = title;
@@ -5,13 +8,22 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 
-  this.info = function () {
-    return `${title} by ${author}, ${pages} pages, ${
+  this.info = () =>
+    `${title} by ${author}, ${pages} pages, ${
       read === true ? "read" : "not read yet"
     }`;
-  };
 }
 
-const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
+// function to add a book to the library
+function addBookToLibrary() {
+  /*   const title = prompt("What is the title?");
+  const author = prompt("Who is the author?");
+  const pages = parseInt(prompt("How many pages?"), 10);
+  const read = prompt("Have you read it?").toLowerCase() === "yes"; */
 
-// console.log(hobbit.info());
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}
+
+addBookToLibrary();
+console.log(myLibrary);
